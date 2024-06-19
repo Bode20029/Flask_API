5693aaf65b4fc622cab10affebc71f9b6bee55a1
Bode20029 <jry.bode@gmail.com>
2024-06-19 13:12:08 +0700

    finished

--- api.py
+++ api.py
@@ -18,10 +18,10 @@ user_args = reqparse.RequestParser()
 user_args.add_argument('name', type=str, required = True, help="Name cannot be blank")
 user_args.add_argument('email', type=str, required = True, help="Email cannot be blank")
 
-ีuseFields = {
+userFields = {
     'id': fields.Integer,
     'name': fields.String,
-    'email: fields.String,
+    'email': fields.String,
 }
 
 class Users(Resource):
@@ -39,8 +39,41 @@ class Users(Resource):
         users = UserModel.query.all()
         return users, 201
 
-api.add_resource(Users, '/api/users/')
+class User(Resource):
+    @marshal_with(userFields)
+    def get(self, id):
+        args = user_args.parse_args()
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404, "User not found")
+        return user
 
+    @marshal_with(userFields)
+    def patch(self, id):
+        args = user_args.parse_args()
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404, "User not found")
+        user.name = args["name"]
+        user.email = args["email"]
+        db.session.commit()
+        return user
+    
+    @marshal_with(userFields)
+    def delete(self, id):
+        
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404, "User not found")
+        db.session.delete(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users#, 204
+    
+    
+
+api.add_resource(Users, '/api/users/')
+api.add_resource(User, '/api/users/<int:id>')
 @app.route('/')
 def home():
     return '<h1>Flask REST API</h1>'
